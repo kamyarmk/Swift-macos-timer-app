bee12690041874356e32f771a2629ded7bcc7a41
kamyar mk <kamiarmk.mk@gmail.com>
2021-09-26 15:38:32 +0300

    Initial Commit

---
+++ test-timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test-timer
+//
+//  Created by Kamyar on 9/26/21.
+//  Copyright © 2021 Kamyar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test-timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test-timer
+//
+//  Created by Kamyar on 9/26/21.
+//  Copyright © 2021 Kamyar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

126c60b67e2f28f547f04f8ec955d20c249e87e7
kamyar mk <kamiarmk.mk@gmail.com>
2021-09-30 12:57:48 +0300

    Added Three Button for Pause and Start and Stop and made some Flags to Control the Flow of the App

--- test-timer/ViewController.swift
+++ test-timer/ViewController.swift
@@ -11,11 +11,13 @@ import Cocoa
 class ViewController: NSViewController {
 
     @IBOutlet weak var CountHolder: NSTextField!
+    @IBOutlet weak var TimersStack: NSStackView!
     
     @IBOutlet weak var CounterFlag: NSButton!
     
     var timer = Timer()
-    var runCount = 0
+    var runCount = 0;
+    var isStart = false;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,20 +32,31 @@ class ViewController: NSViewController {
     }
     
     @objc func SecCounter(timer: TimeInterval){
-        print("Timer fired!")
+        print("Timer Pressed!")
         runCount += 1
         CountHolder.stringValue = String(runCount)
     }
 
     @IBAction func CounterStart(_ sender: Any) {
-        if(CounterFlag.intValue == 1){
+        CounterFlag.intValue = 1;
+        if(!isStart){
             timer = Timer.scheduledTimer(
                 timeInterval: 1.0, target: self, selector: #selector(SecCounter),
                 userInfo: nil, repeats: true)
-        }else{
-            timer.invalidate();
+            isStart = true;
         }
     }
+    @IBAction func CounterPause(_ sender: Any) {
+        CounterFlag.intValue = 1;
+        timer.invalidate();
+        isStart = false;
+    }
     
+    @IBAction func CounterStop(_ sender: Any) {
+        CounterFlag.intValue = 0;
+        timer.invalidate();
+        runCount = 0;
+        isStart = false;
+    }
 }
 
